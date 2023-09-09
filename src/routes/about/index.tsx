ad7b824c5e70542297c20ae27c6a89eeea1eb920
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-09 22:00:56 +0300

    +

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,19 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+        <article>
+            <h2>About</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet perspiciatis, 
+                adipisci praesentium maiores, optio rerum placeat sequi et minus quisquam 
+                error hic dolore fugiat iusto iste ducimus exercitationem libero accusantium.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet perspiciatis, 
+                adipisci praesentium maiores, optio rerum placeat sequi et minus quisquam 
+                error hic dolore fugiat iusto iste ducimus exercitationem libero accusantium.
+            </p>
+        </article>
+    )
+})
