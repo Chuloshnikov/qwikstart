80d3d4f95ba9192f1c02ac046de009cfad5894ee
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-10 16:58:10 +0300

    done

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,15 +1,51 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 
 export default component$(() => {
+    const formVisible = useSignal(false);
+
     return (
-        <article>
-            <h2>Contacts</h2>
-            <p>
-                Nevada, fullhose st. 74
-            </p>
-            <p>
-               +3456529607549495
-            </p>
+        <article
+        class="text-center pt-12 flex flex-col gap-2"
+        >
+            <div>
+                <h2>Contacts</h2>
+                <p>
+                    Nevada, fullhose st. 74
+                </p>
+                <p>
+                +3456529607549495
+                </p>
+                <div>
+                    <button
+                    onClick$={() => formVisible.value = !formVisible.value}
+                    class="px-4 py-2 bg-red-800 text-white"
+                    >
+                        Contact with us
+                </button>
+                </div>
+                
+                    
+                {formVisible.value && (
+                <form>
+                    <div>
+                        <label>Your name:</label>
+                        <input
+                        type="text"
+                        placeholder="your name"
+                        />
+                    </div>
+                    <div>
+                        <label>Your message:</label>
+                        <textarea></textarea>
+                    </div>
+                    <button
+                        type="submit"
+                    >
+                        Send
+                    </button>
+                </form>
+                )}
+            </div>
         </article>
     )
 })
